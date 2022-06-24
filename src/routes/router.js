c95b3d8643659f235a6fd248345252209798d5eb
Ode42 <ode.progr@outlook.com>
2022-06-24 10:50:29 +0300

    Labels router

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const tilanneRouter = require("./tilanneRouter");
+const labelsRouter = require("./labelsRouter");
 
 const router = express.Router();
 
 router.use("/tilanne", tilanneRouter);
+router.use("/labels", labelsRouter);
 
 router.get("/", async (request, response) => {
   response.send("Hello from API");
